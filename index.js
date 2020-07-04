6773db669f802262c14b1171fe24e2a732dc220a
mugtaba-subahi <m_subahi@hotmail.com>
2020-07-04 20:49:49 +0100

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(morgan());
+
 app.get('/', function (req, res) {
-  res.send('This is a Node.js API running on EC2');
+  console.log('log:: GET /');
+  res.send('This is a Node.js API running on EC2: GET /');
 });
 
-app.listen(3000);
+app.get('/skill-store-test', function (req, res) {
+  console.log('log:: GET /skill-store-test');
+  res.send('This is a Node.js API running on EC2: GET /skill-store-test');
+});
+
+app.listen(80);
